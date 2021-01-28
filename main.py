6b7d7fbbbd7a20a9aff73efbbd0c99535d071867
Antonio <Tenundor@gmail.com>
2021-01-28 18:34:37 +0300

    Добавил обработку исключения HTTPError.

--- main.py
+++ main.py
@@ -139,9 +139,11 @@ def main():
     number_of_comics = get_number_xkcd_comics()
     random_comic_id = randint(1, number_of_comics)
     api_version = "5.126"
-    response = post_comic_on_vk_wall(vk_access_token, api_version, vk_group_id,
-                                     random_comic_id)
-    print(response)
+    try:
+        post_comic_on_vk_wall(vk_access_token, api_version, vk_group_id,
+                              random_comic_id)
+    except requests.exceptions.HTTPError as error:
+        print(error)
 
 
 if __name__ == "__main__":
